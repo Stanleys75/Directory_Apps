fcb159f8329ec89131a3d94bfc85a68bd1815302
Stanleys75 <stanleysaputra25@gmail.com>
2025-04-24 19:50:19 +0700

    [Update] Fetching Data, cache and live Api

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,17 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({searchParams}: {
   searchParams : Promise<{query?: string}>
 }) {
 
   const query = (await searchParams).query;
+  const params = {search: query || null};
 
-  const posts = [{
-    _createdAt: new Date(),
-    views: 55,
-    author: {_id: 1, name:'Adrian'},
-    description: 'This is a description',
-    image: '/Picture.jpg',
-    category: "Robots",
-    title: "We Robots"
-
-  }]
+  const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
   return (
     <>
@@ -39,7 +33,7 @@ export default async function Home({searchParams}: {
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post}/>
             ))
           ) : (
@@ -47,6 +41,8 @@ export default async function Home({searchParams}: {
           )}
         </ul>
       </section>
+
+      <SanityLive />
         
     </>
   );
