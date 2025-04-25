67df94911dbba100573e71ed6ab4334b4daddc4a
Stanleys75 <stanleysaputra25@gmail.com>
2025-04-25 14:12:59 +0700

    [Update] Create Author Authentication & Details Startup

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
       protocol: 'https',
       hostname: '**',
     }]
+  },
+  experimental: {
+    ppr: 'incremental',
+  },
+  devIndicators: {
+    position: "bottom-right",
   }
 };
 
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -16,3 +16,38 @@ export const STARTUPS_QUERY =
             image
         }`
     );
+
+    export const STARTUPS_BY_ID_QUERY = defineQuery(
+        `*[_type == "startup" && _id == $id][0]{
+            _id,
+            title,
+            slug,
+            _createdAt,
+            author -> {
+                _id, name, username, image, bio
+            },
+            views,
+            description,
+            category,
+            image,
+            pitch,
+        }`
+    );
+
+    export const STARTUP_VIEW_QUERY = defineQuery(
+        `*[_type == "startup" && _id == $id][0]{
+            _id, views
+        }`
+    )
+
+    export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(
+        `*[_type == "author" && id == $id][0]{
+            _id,
+            id,
+            name,
+            username,
+            email,
+            image,
+            bio
+        }`
+    )
